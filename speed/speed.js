8d66dd04ed973b73795e72683341223cb559a002
kelicaleb <kelicaleb7@gmail.com>
2022-11-28 13:18:40 +0300

    Completed assignment

--- speed/speed.js
+++ speed/speed.js
@@ -1,11 +1,16 @@
-const question = (speed)=>{
-    if(speed <= 70){
-        return "OK"
+/* Called the index.html form*/
+const form = document.querySelector("#speed");
+form.addEventListener("submit", (button)=>{
+/*allowed the data to be submit to the js form*/
+    button.preventDefault()
+    let jsform = new FormData(form);
+/*provided the logic of the data being submitted */
+    if(jsform.get("limit") <= 70){
+        alert("OK")
     }
-/*created a logic behind for every point in above 75*/
-    else if (speed > 70){
-        return "Points:"+((speed-70)/5);
+    else if(jsform.get("limit") > 70){
+        let limit = (jsform.get("limit")-70)/5;
+        console.log(limit)
+        alert(`Points: ${limit}`)
     }
-}
-/* called the function*/
-console.log(question(71))
+})
